929d7b69b518a97b76b059aa81f05c220d22e422
Thanasak <bestzacap20@gmail.com>
2022-07-12 16:57:34 +0700

    fix(ManageUser) use axios for api call

---
+++ src/utils/ManageUser.js
@@ -0,0 +1,64 @@
+import axios from 'axios'
+
+const users_base_url = 'https://authentication-mysql.herokuapp.com'
+
+export const loginUser = async userData => {
+  const res = await axios({
+    method: 'post',
+    url: users_base_url + '/login',
+    headers: { 'Content-Type': 'application/json' },
+    data: JSON.stringify(userData),
+  })
+
+  return res.data
+}
+
+export const regisUser = async userData => {
+  const res = await axios({
+    method: 'post',
+    url: users_base_url + '/register',
+    headers: { 'Content-Type': 'application/json' },
+    data: JSON.stringify(userData),
+  })
+
+  return res.data
+  // fetch('https://authentication-mysql.herokuapp.com/register', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify(jsonData),
+  // })
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     if (data.status) {
+  //       window.location = '/login'
+  //     } else {
+  //       setErrShow(error => (error = 'This username already in use.'))
+  //     }
+  //   })
+  //   .catch(error => {
+  //     console.error('error', error)
+  //   })
+}
+
+export const authenUser = async token => {
+  const res = await axios({
+    method: 'post',
+    url: users_base_url + '/authen',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + token,
+    },
+  })
+
+  return res.data
+}
+
+export const getUser = () => {
+  if (localStorage.username) {
+    return localStorage.getItem('username')
+  }
+
+  return
+}
