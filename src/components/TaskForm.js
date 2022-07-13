11775eb31b8c86f246a987a9541a8fdc568886cb
Thanasak <bestzacap20@gmail.com>
2022-07-13 22:47:18 +0700

    prettier

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,7 +1,7 @@
 import Button from './Button'
 import React, { useState } from 'react'
 
-function TaskForm(props) {
+function TaskForm({ newTask }) {
   const [inputTask, setInputTask] = useState('')
 
   const handleInputChange = e => {
@@ -11,29 +11,27 @@ function TaskForm(props) {
   const handleSubmit = e => {
     e.preventDefault()
     if (inputTask !== '') {
-      props.newTask(inputTask)
+      newTask(inputTask)
       setInputTask('')
     } else return
   }
 
   return (
-    <>
-      <div className="mb-4">
-        <form
-          className="relative flex items-center gap-4 transition-all"
-          onSubmit={handleSubmit}
-        >
-          <input
-            type="text"
-            className="font-light bg-black bg-opacity-10 w-full p-2 rounded-md border-0 outline-none text-black dark:text-white"
-            placeholder="What do you want to do?"
-            value={inputTask}
-            onChange={handleInputChange}
-          />
-          <Button text="Enter" type="submit" />
-        </form>
-      </div>
-    </>
+    <div className="mb-4">
+      <form
+        className="relative flex items-center gap-4 transition-all"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          className="font-light bg-black bg-opacity-10 w-full p-2 rounded-md border-0 outline-none text-black dark:text-white"
+          placeholder="What do you want to do?"
+          value={inputTask}
+          onChange={handleInputChange}
+        />
+        <Button text="Enter" type="submit" />
+      </form>
+    </div>
   )
 }
 
