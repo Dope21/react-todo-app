32816421cafb4663b550ef682e2205ae177ea11d
Thanasak <bestzacap20@gmail.com>
2022-07-13 22:51:45 +0700

    update(unshift task,trashbutton component,loop tasks)

--- src/components/pages/Task.js
+++ src/components/pages/Task.js
@@ -1,75 +1,73 @@
-import { TrashIcon } from '@heroicons/react/outline'
 import React, { useState, useEffect } from 'react'
 import TaskForm from '../TaskForm'
-import TaskList from '../TaskList'
+import TaskItem from '../TaskItem'
+import TrashButton from '../TrashButton'
 
 function Task() {
-
   const [delState, setDelState] = useState(false)
-  const [tasks, setTasks] = useState( () => {
-      const saveTasks = localStorage.getItem('tasks') 
-      if (saveTasks) {
-        return JSON.parse(saveTasks)
-      } else {
-        return []
-      }
+  const [tasks, setTasks] = useState(() => {
+    const saveTasks = localStorage.getItem('tasks')
+    if (saveTasks) {
+      return JSON.parse(saveTasks)
+    } else {
+      return []
     }
-  )
+  })
 
-  const addNewTask = (newTask) => {
+  const addNewTask = newTask => {
     const task = {
       id: Math.floor(Math.random() * 10000),
       state: false,
-      text: newTask
+      text: newTask,
     }
-    const allTasks = [task, ...tasks]
-    setTasks( tasks => tasks = allTasks)
+    const allTasks = [...tasks]
+    allTasks.unshift(task)
+    setTasks(tasks => (tasks = allTasks))
   }
 
-  const updateTask = (id) => {
+  const updateTask = id => {
     let updateTask = tasks.map(task => {
       if (task.id === id) {
         task.state = !task.state
       }
       return task
     })
-    setTasks(tasks => tasks = updateTask.sort((a, b) => a.state - b.state))
+    setTasks(tasks => (tasks = updateTask.sort((a, b) => a.state - b.state)))
   }
 
-  const deleteTask = (id) => {
-    let updateTask = tasks.filter(task => task.id !== id )
-    setTasks(tasks => tasks = updateTask)
+  const deleteTask = id => {
+    let updateTask = tasks.filter(task => task.id !== id)
+    setTasks(tasks => (tasks = updateTask))
   }
 
   const delToggle = () => {
-    setDelState( delState => delState = !delState )
+    setDelState(!delState)
   }
 
-  useEffect( () => {
+  useEffect(() => {
     localStorage.setItem('tasks', JSON.stringify(tasks))
-  },[tasks])
+  }, [tasks])
 
   return (
     <>
-      <div className='flex flex-col overflow-hidden min-h-screen '>
-        <TaskForm newTask={addNewTask}/>
-        <TaskList 
-          allTasks={tasks}
-          taskState={updateTask}
-          delState={delState}
-          delTask={deleteTask}
-        />
+      <div className="flex flex-col overflow-hidden min-h-screen ">
+        <TaskForm newTask={addNewTask} />
+        <ul className="flex flex-col gap-4">
+          {tasks.length !== 0 &&
+            tasks.map(task => {
+              return (
+                <TaskItem
+                  task={task}
+                  taskState={updateTask}
+                  delButton={delState}
+                  delTask={deleteTask}
+                  key={task.id}
+                />
+              )
+            })}
+        </ul>
       </div>
-      <div className='flex justify-center fixed bottom-0 left-0 w-full p-7'>
-        <div className='max-w-xl flex justify-end w-full'>
-          <div 
-            className='bg-purple-main rounded-full text-white w-[60px] h-[60px] flex items-center justify-center cursor-pointer'
-            onClick={delToggle}
-          >
-            <TrashIcon className='w-7'/>
-          </div>
-        </div>
-      </div>
+      <TrashButton onClick={delToggle} />
     </>
   )
 }
