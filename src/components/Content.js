0b0fb0ce1b250c081597726b188c68c9afbee942
Thanasak <bestzacap20@gmail.com>
2022-07-19 22:27:19 +0700

    fixed min-high 575px -> sreen

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Content({ children }) {
   return (
     <>
-      <main className="bg-white dark:bg-purple-dark rounded-t-xl min-h-[575px] px-7 pt-7 pb-28">
+      <main className="bg-white dark:bg-purple-dark rounded-t-xl min-h-screen px-7 pt-7 pb-28">
         <div className="h-full max-w-xl mx-auto">{children}</div>
       </main>
     </>
